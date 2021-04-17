fd79009a0b736eb7a43d87d44718b1429ff320f8
Andrew Petrov <marsofandrew@gmail.com>
2021-04-17 23:28:26 +0300

    13 - test: remove unused imports

--- src/test/java/ru/spbstu/repository/AnnotationRepositoryTest.java
+++ src/test/java/ru/spbstu/repository/AnnotationRepositoryTest.java
@@ -1,17 +1,13 @@
 package ru.spbstu.repository;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.spbstu.model.Annotation;
 import ru.spbstu.model.Variant;
 
