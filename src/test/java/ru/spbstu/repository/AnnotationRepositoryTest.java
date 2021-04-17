8edf8d21becff5f4c824331cbd578c4d69bee360
Andrew Petrov <marsofandrew@gmail.com>
2021-04-17 23:24:11 +0300

    13 - style: use 4 spaces instead of 2 for indents

--- src/test/java/ru/spbstu/repository/AnnotationRepositoryTest.java
+++ src/test/java/ru/spbstu/repository/AnnotationRepositoryTest.java
@@ -23,96 +23,96 @@ import java.util.Optional;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AnnotationRepositoryTest {
-  @Autowired
-  private VariantRepository variantRepository;
-  @Autowired
-  private AnnotationRepository annotationRepository;
+    @Autowired
+    private VariantRepository variantRepository;
+    @Autowired
+    private AnnotationRepository annotationRepository;
 
-  @Test
-  public void simpleInsert() {
-    Variant variant = variantRepository.save(new Variant()
-        .setChromosome("a")
-        .setPosition(2L)
-        .setReferenceBase("aaa"));
+    @Test
+    public void simpleInsert() {
+        Variant variant = variantRepository.save(new Variant()
+                .setChromosome("a")
+                .setPosition(2L)
+                .setReferenceBase("aaa"));
 
-    Annotation info = annotationRepository.save(new Annotation()
-        .setVariant(variant)
-        .setInfo(Map.of("info", "1")));
+        Annotation info = annotationRepository.save(new Annotation()
+                .setVariant(variant)
+                .setInfo(Map.of("info", "1")));
 
-    Optional<Annotation> variantInformation = annotationRepository.findById(info.getId());
-    Assertions.assertThat(variantInformation)
-        .isPresent();
-    Assertions.assertThat(variantInformation.get().getVariant())
-        .isEqualTo(variant);
-  }
+        Optional<Annotation> variantInformation = annotationRepository.findById(info.getId());
+        Assertions.assertThat(variantInformation)
+                .isPresent();
+        Assertions.assertThat(variantInformation.get().getVariant())
+                .isEqualTo(variant);
+    }
 
-  @Test
-  public void violateConstraintsJsonb(){
-      Variant variant = variantRepository.save(new Variant()
-              .setChromosome("a")
-              .setPosition(2L)
-              .setReferenceBase("aaa"));
-      Assertions.assertThatThrownBy(() -> annotationRepository.save(new Annotation().setVariant(variant)))
-              .isInstanceOf(DataIntegrityViolationException.class);
-  }
+    @Test
+    public void violateConstraintsJsonb() {
+        Variant variant = variantRepository.save(new Variant()
+                .setChromosome("a")
+                .setPosition(2L)
+                .setReferenceBase("aaa"));
+        Assertions.assertThatThrownBy(() -> annotationRepository.save(new Annotation().setVariant(variant)))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
 
-  @Test
-  public void violateConstraintsVariantIdNotExists() {
-    Assertions.assertThatThrownBy(() -> annotationRepository.save(new Annotation().setVariant(new Variant()
-            .setId(1L)).setInfo(Map.of("info", "2"))))
-            .isInstanceOf(DataIntegrityViolationException.class);
-  }
+    @Test
+    public void violateConstraintsVariantIdNotExists() {
+        Assertions.assertThatThrownBy(() -> annotationRepository.save(new Annotation().setVariant(new Variant()
+                .setId(1L)).setInfo(Map.of("info", "2"))))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
 
-  @Test
-  public void  violateConstraintsVariantIdNotSet(){
-      Assertions.assertThatThrownBy(() -> annotationRepository.save(new Annotation().setInfo(Map.of("info", "1"))))
-              .isInstanceOf(DataIntegrityViolationException.class);
-  }
+    @Test
+    public void violateConstraintsVariantIdNotSet() {
+        Assertions.assertThatThrownBy(() -> annotationRepository.save(new Annotation().setInfo(Map.of("info", "1"))))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
 
-  @Test
-  public void findAllByVariantId() {
-    Variant variant = variantRepository.save(new Variant()
-        .setChromosome("a")
-        .setPosition(2L)
-        .setReferenceBase("aaa"));
-    Variant variant2 = variantRepository.save(new Variant()
-        .setChromosome("a")
-        .setPosition(3L)
-        .setReferenceBase("aaa"));
-    annotationRepository.saveAll(List.of(
-        new Annotation().setVariant(variant).setInfo(Map.of("info", "1")),
-        new Annotation().setVariant(variant2).setInfo(Map.of("info", "2")),
-        new Annotation().setVariant(variant).setInfo(Map.of("info", "3"))));
+    @Test
+    public void findAllByVariantId() {
+        Variant variant = variantRepository.save(new Variant()
+                .setChromosome("a")
+                .setPosition(2L)
+                .setReferenceBase("aaa"));
+        Variant variant2 = variantRepository.save(new Variant()
+                .setChromosome("a")
+                .setPosition(3L)
+                .setReferenceBase("aaa"));
+        annotationRepository.saveAll(List.of(
+                new Annotation().setVariant(variant).setInfo(Map.of("info", "1")),
+                new Annotation().setVariant(variant2).setInfo(Map.of("info", "2")),
+                new Annotation().setVariant(variant).setInfo(Map.of("info", "3"))));
 
-    List<Annotation> infos = annotationRepository.findAllByVariant_Id(variant.getId());
-    Assertions.assertThat(infos)
-        .hasSize(2);
-    Assertions.assertThat(infos.stream().map(Annotation::getInfo).map(m -> m.get("info")))
-        .containsOnly("1", "3");
-  }
+        List<Annotation> infos = annotationRepository.findAllByVariant_Id(variant.getId());
+        Assertions.assertThat(infos)
+                .hasSize(2);
+        Assertions.assertThat(infos.stream().map(Annotation::getInfo).map(m -> m.get("info")))
+                .containsOnly("1", "3");
+    }
 
-  @Test
-  public void checkFindByVariantCodes() {
-    Variant variant = variantRepository.save(new Variant()
-        .setChromosome("a")
-        .setPosition(2L)
-        .setReferenceBase("aaa"));
-    Variant variant2 = variantRepository.save(new Variant()
-        .setChromosome("a")
-        .setPosition(3L)
-        .setReferenceBase("aaa"));
-    Variant variant3 = variantRepository.save(new Variant()
-        .setChromosome("a")
-        .setPosition(4L)
-        .setReferenceBase("bbb"));
-    annotationRepository.saveAll(List.of(
-        new Annotation().setVariant(variant).setInfo(Map.of("info", "1")),
-        new Annotation().setVariant(variant2).setInfo(Map.of("info", "2")),
-        new Annotation().setVariant(variant3).setInfo(Map.of("info", "3"))));
+    @Test
+    public void checkFindByVariantCodes() {
+        Variant variant = variantRepository.save(new Variant()
+                .setChromosome("a")
+                .setPosition(2L)
+                .setReferenceBase("aaa"));
+        Variant variant2 = variantRepository.save(new Variant()
+                .setChromosome("a")
+                .setPosition(3L)
+                .setReferenceBase("aaa"));
+        Variant variant3 = variantRepository.save(new Variant()
+                .setChromosome("a")
+                .setPosition(4L)
+                .setReferenceBase("bbb"));
+        annotationRepository.saveAll(List.of(
+                new Annotation().setVariant(variant).setInfo(Map.of("info", "1")),
+                new Annotation().setVariant(variant2).setInfo(Map.of("info", "2")),
+                new Annotation().setVariant(variant3).setInfo(Map.of("info", "3"))));
 
-    List<Annotation> annotations = annotationRepository.findAllByVariant_VariantCodeIn(List.of("a:2:aaa>", "a:4:bbb>"));
-    Assertions.assertThat(annotations).hasSize(2);
-    Assertions.assertThat(annotations.stream().map(Annotation::getVariant).map(Variant::getId))
-        .containsOnly(variant.getId(), variant3.getId());
-  }
+        List<Annotation> annotations = annotationRepository.findAllByVariant_VariantCodeIn(List.of("a:2:aaa>", "a:4:bbb>"));
+        Assertions.assertThat(annotations).hasSize(2);
+        Assertions.assertThat(annotations.stream().map(Annotation::getVariant).map(Variant::getId))
+                .containsOnly(variant.getId(), variant3.getId());
+    }
 }
