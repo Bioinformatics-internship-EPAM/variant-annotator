0f0b0b0bf590e1130de6cf0fe84f5b75f94edb51
Egor Shakhmin <lapter57@gmail.com>
2021-04-18 15:58:34 +0300

    18 feat: calculate code coverage with jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,13 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+
 plugins {
     java
     id("org.springframework.boot") version "2.4.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.sonarqube") version "3.1.1"
+    id("jacoco")
 }
 
 group = "ru.spbstu"
@@ -10,6 +15,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     jcenter()
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 configure<JavaPluginConvention> {
@@ -36,11 +42,53 @@ dependencies {
     testAnnotationProcessor(lombok)
 }
 
-sonarqube {
-    properties {
-        property("sonar.projectName", "variant-annotator")
-        property("sonar.projectKey", "Bioinformatics-internship-EPAM_variant-annotator")
-        property("sonar.host.url", "https://sonarcloud.io")
+jacoco {
+    toolVersion = "0.8.7-SNAPSHOT"
+}
+
+tasks {
+    jacocoTestReport {
+        dependsOn(test)
+
+        executionData(fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec"))
+
+        subprojects.forEach {
+            sourceSets(it.sourceSets["main"])
+        }
+
+        reports {
+            listOf(xml, html).forEach { report ->
+                report.isEnabled = true
+                report.destination = file("$buildDir/reports/jacoco/coverage.${report.name}")
+            }
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+
+        testLogging {
+            events(PASSED, SKIPPED, FAILED)
+        }
+
+        finalizedBy(jacocoTestReport)
+    }
+
+    sonarqube {
+        properties {
+            property("sonar.host.url", "https://sonarcloud.io")
+            property("sonar.organization", "bioinformatics-internship")
+            property("sonar.projectKey", "Bioinformatics-internship-EPAM_variant-annotator")
+            property("sonar.projectName", "variant-annotator")
+            property("sonar.tests", "src/test")
+            property("sonar.binaries", "${project.buildDir}/classes")
+            property("sonar.sources", "src/main")
+            property("sonar.language", "java")
+            property("sonar.junit.reportPaths", "${project.buildDir}/test-results")
+            property("sonar.dynamicAnalysis", "reuseReports")
+            property("sonar.java.coveragePlugin", "jacoco")
+            property("sonar.coverage.jacoco.xmlReportPaths", "${project.buildDir}/reports/jacoco/coverage.xml")
+        }
     }
 }
 
