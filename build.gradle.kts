bf19757eb0f305bbe552ec61ce59dd28a3a0f961
Roman Egel <40761428+RomanEgel@users.noreply.github.com>
2021-05-05 19:14:37 +0300

    #10 connect swagger (#16)
    10 feat: connect swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
     implementation("com.vladmihalcea:hibernate-types-52:2.10.3")
     implementation("com.google.guava:guava:30.1.1-jre")
     implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
